082d9d54108caef0e2daa7035c8452f779a149a2
Jiwon Park <60731177+jiwon4178@users.noreply.github.com>
2024-11-19 21:41:15 +0900

    [level 3] Title: 네트워크, Time: 4.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,40 +1,52 @@
-def dfs(n, computers, start, visited):
-    stack = [start] # 이러면 뭐가되는거임...?
-    while stack:
-        com = stack.pop()
-        if com not in visited:
-            visited.append(com)
-            for connect in range(n):
-                if connect != com and computers[com][connect] == 1 and (connect not in visited):
-                    stack.append(connect)
+# def dfs(n, computers, start, visited):
+#     stack = [start] 
+#     while stack:
+#         com = stack.pop()
+#         visited.append(com)
+#         for connect in range(n):
+#             if connect != com and computers[com][connect] == 1 and (connect not in visited):
+#                 stack.append(connect)
 
+
+
+# def solution(n, computers):
+#     answer = 0
+#     # 리스트 정의 
+#     visited = list()
+    
+    
+#     for com in range(n):
+#         if com not in visited:
+#             dfs(n, computers, com, visited)
+#             answer += 1
+#     return answer
+
+from collections import deque
+def bfs(n, computers, start, visited):
+    visited[start] = True # 방문 표시 
+    queue = deque([start])
+    
+    while queue:
+        com = queue.popleft()
+        if visited[com] != True: # 방문안했으면 
+            visited[com] = True # 방문처리하고 
+        for connect in range(n):
+            if connect != com and computers[com][connect] == 1 and visited[connect] != True:
+                queue.append(connect)
+        # print(queue)
 
 
 def solution(n, computers):
     answer = 0
     
-    # 리스트 정의 
-    visited = list()
-    
+    # 방문 노드 정의 
+    visited = [False for _ in range(n)]
     
     for com in range(n):
-        if com not in visited:
-            dfs(n, computers, com, visited)
+        if visited[com] != True: # 방문 안했으면 
+            bfs(n, computers, com, visited)
             answer += 1
     return answer
             
             
             
-        
-        
-    to_visit.append(0)
-    
-    while to_visit: 
-        node = to_visit.pop()
-        
-        if node not in visited:
-            visited.append(node)
-            to_visit.extend(computers[node])
-    
-    
-    return answer
