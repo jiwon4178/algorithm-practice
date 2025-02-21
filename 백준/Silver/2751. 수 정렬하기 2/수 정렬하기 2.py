a4186fa9508c1cb50be12006cb65f7dd18b88a65
Jiwon Park <60731177+jiwon4178@users.noreply.github.com>
2025-02-21 21:38:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1388 ms, Memory: 78268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,49 +1,14 @@
-import sys
+import sys 
 
-def merge(arr, low, high):
-    temp = []
-    mid = (low + high) // 2
-    i, j = low, mid + 1
+# 기본 내장함수로 풀기 
 
-    while i <= mid and j <= high:
-        if arr[i] <= arr[j]:
-            temp.append(arr[i])
-            i += 1
-        else:
-            temp.append(arr[j])
-            j += 1
+N = int(input())
 
-    while i <= mid:
-        temp.append(arr[i])
-        i += 1
+array = []
+for i in range(N):
+    array.append(int(sys.stdin.readline()))
 
-    while j <= high:
-        temp.append(arr[j])
-        j += 1
+array.sort() # or # sorted(array) 사용 
 
-    for k in range(low, high + 1):
-        arr[k] = temp[k - low]
-
-    return arr
-
-def merge_sort(arr, low, high):
-    if low >= high:
-        return
-
-    mid = (low + high) // 2
-
-    merge_sort(arr, low, mid)
-    merge_sort(arr, mid + 1, high)
-
-    merge(arr, low, high)
-
-# 입력 받기
-N = int(sys.stdin.readline().strip())
-arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
-
-# 병합 정렬 실행
-merge_sort(arr, 0, N - 1)
-
-# 정렬된 결과 출력
-for num in arr:
-    print(num)
+for i in range(N):
+    print(array[i])
